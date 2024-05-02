9d6e61b91d7701dfd35e8864661c42ff9f73db64
Kenichi Kamiya <kachick1@gmail.com>
2024-05-02 12:48:53 +0900

    Refine how to keep compatibility in multiple irb versions (#198)
    * Refine how to keep compatible multiple irb versions
    
    * Test against supporting oldest dependencies
    
    * Remove unused debug gem from development
    
    * Drop older irb versions
    
    IRB::Command is introduced since v1.12.0
    https://github.com/ruby/irb/commit/462c1284af0e6d4c4177a6dd30ed3dfc84f303db
    
    * Relax to bump dev dependencies

--- lib/irb/power_assert.rb
+++ lib/irb/power_assert.rb
@@ -5,15 +5,6 @@
 require 'irb'
 require 'power_assert'
 
-module IRB
-  module PowerAssert
-    def self.newer_irb?
-      # This assumes irb bumps minor or major in next versions, because they do not bump in current development versions.
-      Gem::Version.create(IRB::VERSION) >= Gem::Version.create('1.13.0') || RUBY_VERSION >= '3.4'
-    end
-  end
-end
-
 module PowerAssert
   INTERNAL_LIB_DIRS[IRB::PowerAssert] = File.dirname(File.dirname(caller_locations(1, 1).first.path))
 end
